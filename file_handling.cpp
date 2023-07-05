d711670872302e48cd198e03b60fad417f2f8e4d
Gajula Ajay <94693863+20BCS6140@users.noreply.github.com>
2023-07-05 16:09:05 +0530

    file handling

--- file_handling.cpp
+++ file_handling.cpp
@@ -6,22 +6,42 @@ using namespace std;
 
 int main()
 {
-    // cout << "Enter the str: ";
+    // one way to write into a file.
     // string str;
-    
-
+    // cout << "Enter the str: ";
     // getline(cin, str);
-
     // ofstream out("sample.txt");
-
     // out<<str;
+    // out.close();
+
+    // Anoher way to write into a file.
+    // string str;
+    // cout << "Enter the str: ";
+    // getline(cin, str);
+    // ofstream out;
+    // out.open("sample,txt");
+    // out << "My name is "+str;
+    // out.close();
+
+
+    // string str1;
+    // ifstream in("sample.txt");
+    // getline(in, str1);
+    // in.close();
+    // cout << str1 << endl;
+
 
     string str1;
+    ifstream in;
+    in.open("sample.txt");
+    getline(in,str1);
+    in.close()
 
-    ifstream in("sample.txt");
-    getline(in, str1);
-
-    cout << str1 << endl;
+    while(in.eof())
+    {
+        getline(in,str1);
+        cout << str1 << endl;
+    }
 
 
 
