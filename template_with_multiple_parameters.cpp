25596b071f662dc643f323dbd59744671443b1b8
Gajula Ajay <94693863+20BCS6140@users.noreply.github.com>
2023-07-05 20:22:44 +0530

    templates

---
+++ template_with_multiple_parameters.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+using namespace std;
+
+template <class T1, class T2> // Multi Parameterized template
+//template <class T1> Single parameterized template
+// template <class T1 = int,  class T2 = float> Default parameterized template
+
+class example
+{
+    T1 data1;
+    T2 data2;
+
+    public:
+
+    example(T1 a, T2 b):data1(a),data2(b){}
+
+    void display()
+    {
+        cout << data1 << " " << data2 << endl;
+    }
+};
+
+int main()
+{
+
+    example <int,char> obj(5,101);
+
+    obj.display();
+
+    T1 ajay ="Ajay";
+    cout << ajay << endl;
+
+}
