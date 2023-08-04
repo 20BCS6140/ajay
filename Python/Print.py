1293bf7b5f6de609b6d1fa0018ca8a7c12a757b5
Gajula Ajay <94693863+20BCS6140@users.noreply.github.com>
2023-08-04 18:07:54 +0530

    Changes

---
+++ Python/Print.py
@@ -0,0 +1,17 @@
+print("Helllow", end=" ")
+print(5)
+print("hello", 5)
+print(5*10)
+
+print(5,10,20,sep=",")
+print("My name is ajay\nI am very good boy.")
+
+
+'''We can give multi live comments
+    using triple single quotes ot triple double quotes'''
+
+print("I am 'Ajay'")
+
+print('''My name is "Gajula ajay"''')
+
+print("My name is \"Gajula Ajay\" \nand I\'m good boy.")
---
+++ Python/data_types.py
@@ -0,0 +1,27 @@
+integer = 10
+
+print(type(integer), integer)
+
+
+str = "Ajay"
+print(type(str), str) 
+
+ch = 'A'
+print(type(ch), ch)
+
+boolean = True
+print(type(boolean), boolean)
+
+
+float_Int = 81.05
+print(type(float_Int), float_Int)
+
+nn = None
+
+print(type(nn), nn)
+
+complex_numbers = complex(8,2)
+print(type(complex_numbers), complex_numbers)
+
+print("Adding complex numbers: ", complex_numbers+integer)
+
