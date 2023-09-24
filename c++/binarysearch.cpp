33b823712dc388a3b05363027c84859034efb764
Gajula Ajay <94693863+20BCS6140@users.noreply.github.com>
2023-09-24 17:26:12 +0530

    binarysearch

---
+++ c++/binarysearch.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int binary_search(int *arr,int l, int r, int key)
+{
+    if(l <= r)
+    {
+        int mid = l + (r - l) / 2;;
+
+        if(arr[mid] == key)
+            return mid;
+        else if(arr[mid] < key)
+            return binary_search(arr,mid+1,r,key);
+        else if(arr[mid] > key)
+            return binary_search(arr,l,mid-1,key);
+    }
+    
+    
+    return -1;
+}
+
+int main()
+{
+    int arr_size;
+    int *arr;
+    cout << "Enter array size: ";
+    cin >> arr_size;
+
+    arr = new int[arr_size];
+
+    cout << "Enter " << arr_size << " elements by giving space: ";
+
+    for(int i = 0; i < arr_size; i++)
+    {
+        cin >> arr[i];
+    }
+
+    sort(arr,arr+arr_size);
+
+
+    int key;
+    cout << "Enter the element you want to search: ";
+    cin >> key;
+
+    int result = binary_search(arr, 0, arr_size,key);
+
+    if(result < 0)
+        cout << "Not found." << endl;
+    else
+        cout << "Element found at " << result+1 << endl;
+}
