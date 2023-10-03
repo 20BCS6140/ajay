846aa8b001553a8fa49a1e6fc19b4a1fd24395f5
Gajula Ajay <94693863+20BCS6140@users.noreply.github.com>
2023-10-03 17:20:57 +0530

    mergesort

--- c++/mergesort.cpp
+++ c++/mergesort.cpp
@@ -1,42 +1,42 @@
 #include <iostream>
-#include <vector>
 
 using namespace std;
 
-
-void merge(int arr[], int l, int m, int r)
+void merge(int *arr, int l, int m, int r)
 {
+    int i = l, j = m+1, k=l;
 
-    int i = l;
-    int k = l;
-    int j = m+1;
-
-    int temp[r];
+    int *temp;
+    temp = new int[r-l];
 
     while(i <= m && j <= r)
     {
         if(arr[i] <= arr[j])
         {
             temp[k] = arr[i];
-            i++,k++;
+            k++;
+            i++;
         }
-        else 
+        else
         {
             temp[k] = arr[j];
-            j++, k++;
+            j++;
+            k++;    
         }
     }
 
     while(i <= m)
     {
         temp[k] = arr[i];
-        i++,k++;
+        k++;
+        i++;
     }
 
     while(j <= r)
     {
         temp[k] = arr[j];
-        j++, k++;
+        j++;
+        k++; 
     }
 
     for(int p = l; p <= r; p++)
@@ -44,37 +44,39 @@ void merge(int arr[], int l, int m, int r)
         arr[p] = temp[p];
     }
 
-
-
 }
 
-void mergesort(int arr[], int l, int r)
+
+void mergesort(int *arr, int l, int r)
 {
     int m = (l+r)/2;
-
     if(l < r)
     {
         mergesort(arr,l,m);
-
         mergesort(arr,m+1,r);
-
         merge(arr,l,m,r);
     }
-
 }
 
 int main()
 {
-    int arr[] =  {85,45,95,5,23,78};
+    int *arr;
+    int size;
+    cout << "Enter the size: ";
+    cin >> size;
+    arr = new int[size];
 
-    int size = sizeof(arr)/sizeof(arr[0]);
-
-    mergesort(arr,0,size-1);
-
+    cout << "Enter the elements: ";
     for(int i = 0; i < size; i++)
     {
-        cout << arr[i] << " ";
+        cin >> arr[i];
     }
-    cout << endl;
 
+    mergesort(arr,0,size-1);
+    cout << endl;
+    
+    for(int i = 0; i < size; i++)
+    {
+        cout <<  arr[i];
+    }
 }
