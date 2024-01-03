c7a289eee32d2c5eaf80c317f2021e5ab67168ca
Gajula Ajay <94693863+20BCS6140@users.noreply.github.com>
2024-01-04 03:18:40 +0530

    random quote generator

---
+++ random_quote_generator/quote.js
@@ -0,0 +1,63 @@
+const quotetext = document.getElementById("advice");
+const authortext = document.getElementById("authorname");
+btn = document.querySelector("button");
+
+twitterBtn = document.querySelector(".twitter");
+
+instaBtn = document.querySelector(".instagram");
+
+soundBtn = document.querySelector(".sound");
+
+const api_url = "https://api.quotable.io/quotes/random";
+
+async function getquote(url)
+{
+    // btn.innerText = "Loading Quote...";
+    // fetch("https://api.quotable.io/quotes").then(res => res.json()).then(result => {
+    // console.log(result.content);
+    // quotetext.innerText = result[0].content;
+    // authortext.innerText = result[0].author;
+
+    // btn.innerText = "New Quote";
+
+    // });
+
+    btn.innerText = "Loading Quote...";
+
+    var response = await fetch(url);
+
+    var data = await response.json();
+    console.log(data);
+    quotetext.innerText = data[0].content;
+    authortext.innerText =  data[0].author;
+
+    btn.innerText = "New Quote";
+    
+}
+
+btn.addEventListener("click",getquote(api_url));
+
+twitterBtn.addEventListener("click", () => {
+
+    window.open("https://twitter.com/intent/tweet?text=" + quotetext.innerText, "_blank");
+
+});
+
+
+instaBtn.addEventListener("click", () => {
+
+    window.open("https://www.instagram.com/", "_blank");
+
+});
+
+soundBtn.addEventListener("click", () => {
+
+    let speech = new SpeechSynthesisUtterance(quotetext.innerText + " by " + authortext.innerText);
+    speechSynthesis.speak(speech);
+
+});
+
+
+
+
+
