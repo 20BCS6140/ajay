646e98759a8f43c0d683520731c1944651458cea
Gajula Ajay <94693863+20BCS6140@users.noreply.github.com>
2024-01-05 01:14:01 +0530

    updated quote

--- random_quote_generator/quote.js
+++ random_quote_generator/quote.js
@@ -1,3 +1,9 @@
+document.addEventListener('DOMContentLoaded', function () {
+    getquote();
+});
+
+
+
 const quotetext = document.getElementById("advice");
 const authortext = document.getElementById("authorname");
 btn = document.querySelector(".new-quote-button");
@@ -10,31 +16,31 @@ instaBtn = document.querySelector(".instagram");
 
 soundBtn = document.querySelector(".sound");
 
-const api_url = "https://api.quotable.io/quotes/random";
+// const api_url = "https://api.quotable.io/quotes/random";
 // const author_api_url = "https://api.quotable.io/quotes?author=";
 
-async function getquote(url)
+function getquote()
 {
-    // btn.innerText = "Loading Quote...";
-    // fetch("https://api.quotable.io/quotes").then(res => res.json()).then(result => {
-    // console.log(result.content);
-    // quotetext.innerText = result[0].content;
-    // authortext.innerText = result[0].author;
-
-    // btn.innerText = "New Quote";
-
-    // });
-
     btn.innerText = "Loading Quote...";
-
-    var response = await fetch(url);
-
-    var data = await response.json();
-    console.log(data);
-    quotetext.innerText = data[0].content;
-    authortext.innerText =  data[0].author;
+    fetch("https://api.quotable.io/quotes/random").then(res => res.json()).then(result => {
+    console.log(result[0]);
+    quotetext.innerText = result[0].content;
+    authortext.innerText = result[0].author;
 
     btn.innerText = "New Quote";
+
+    });
+
+    // btn.innerText = "Loading Quote...";
+
+    // var response = await fetch(url);
+
+    // var data = await response.json();
+    // console.log(data);
+    // quotetext.innerText = data[0].content;
+    // authortext.innerText =  data[0].author;
+
+    // btn.innerText = "New Quote";
     
 }
 
@@ -42,16 +48,22 @@ async function getquote(url)
 {
     const author_input = document.querySelector(".authorsearch").value;
 
-    fetch("https://api.quotable.io/quotes?author=${author_input}").then(res => res.json()).then(result => {
+    urli = "https://api.quotable.io/quotes?author="+author_input;
 
-        console.log(result);
+    fetch("https://api.quotable.io/quotes?author="+author_input)
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+        // quotetext.innerText = data.results[0].content;
+        // authortext.innerText = data.results[0].author;
+
 
     });
     
 
 }
 
-btn.addEventListener("click",getquote(api_url));
+btn.addEventListener("click",getquote);
 
 searchauthorBtn.addEventListener("click",search_author);
 
