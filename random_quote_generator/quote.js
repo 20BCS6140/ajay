02d6bfe3d5a548ad173aa467fa2242cb25b06047
Gajula Ajay <94693863+20BCS6140@users.noreply.github.com>
2024-01-05 19:44:09 +0530

    update

--- random_quote_generator/quote.js
+++ random_quote_generator/quote.js
@@ -8,6 +8,8 @@ const quotetext = document.getElementById("advice");
 const authortext = document.getElementById("authorname");
 btn = document.querySelector(".new-quote-button");
 
+let randomauthorname ="";  //-------------------------raja shekar's
+
 const searchauthorBtn = document.querySelector(".search-author");
 
 twitterBtn = document.querySelector(".twitter");
@@ -26,6 +28,7 @@ function getquote()
     console.log(result[0]);
     quotetext.innerText = result[0].content;
     authortext.innerText = result[0].author;
+    randomauthorname = result[0].author;
 
     btn.innerText = "New Quote";
 
@@ -53,7 +56,15 @@ function getquote()
     fetch("https://api.quotable.io/quotes?author="+author_input)
     .then(response => response.json())
     .then(data => {
-        console.log(data);
+
+        if(data.results.length < 0 || author_input == "")
+        {
+            
+            console.log("No Author Found");
+        }
+        else{
+            console.log(data);   
+        }
         // quotetext.innerText = data.results[0].content;
         // authortext.innerText = data.results[0].author;
 
@@ -62,6 +73,57 @@ function getquote()
     
 
 }
+
+
+
+
+
+
+
+// ---------------------------Raja shekars--------------------------------------
+
+const showspecificauthor = document.getElementById("author_name");
+showtext = document.querySelector(".para");
+
+
+async function getauthor()
+{
+    // alert("hII");
+
+    fetch("https://api.quotable.io/quotes?author="+randomauthorname)
+    .then(response => response.json())
+    .then(data => {
+
+        console.log(randomauthorname);
+        // console.log(data);
+        let text ="";
+        for(let i = 0; i < data.results.length;i++)
+        {
+            // console.log(data.results[i].content);
+            text += `<p>${data.results[i].content}<br> <p>` ;
+
+        }
+
+        console.log(text);
+        showtext.innerText = text;
+
+        
+
+
+    });
+
+}
+
+// showspecificauthor.addEventListener("click",getauthor, () => {
+//     window.open("test.html","_blank");
+// });
+
+
+//--------------------------Raja Sekhar's-----------------------------------
+
+
+
+
 
 btn.addEventListener("click",getquote);
 
